38d607a7e115f7bf765d6914edd038b02bb58149
unknown <kasper1116.kh@gmail.com>
2023-10-26 22:28:07 +0800

    宣告Demo及修改composer沒改到的東西

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Aloi\\LaraAuto\\' => array($baseDir . '/src'),
+    'Demo\\' => array($baseDir . '/src/Demo'),
 );
